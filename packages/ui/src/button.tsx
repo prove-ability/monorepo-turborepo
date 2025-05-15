b3cfd05ee84bc7e7d6f50497eaf08903eea47171
prove-ability <prove.ability00@gmail.com>
2025-05-15 22:52:16 +0900

    init project

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -14,7 +14,8 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
-      {children}
+      q23
+      {children}123
     </button>
   );
 };
