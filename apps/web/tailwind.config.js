04d835213513a20c524354530cba7d7e9e78d149
prove-ability <prove.ability00@gmail.com>
2025-06-15 22:55:37 +0900

    ref: Remove unused Tailwind CSS and PostCSS configurations; update package dependencies

--- apps/web/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-
-import sharedConfig from '@repo/tailwind-config/tailwind.config.js';
-
-/** @type {import('tailwindcss').Config} */
-const config = {
-  presets: [sharedConfig],
-};
-
-export default config;
