3733d0a3f9ac1be1a5569099cf19ce6103efac0f
prove-ability <prove.ability00@gmail.com>
2025-06-29 23:37:30 +0900

    feat: 새로운 컴포넌트 및 스타일 추가로 대시보드 기능 확장

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -51,12 +51,9 @@ export default function RootLayout({
               <div className="flex flex-1 flex-col">
                 <div className="@container/main flex flex-1 flex-col gap-2">
                   <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                    {/* <SectionCards />
-                    <div className="px-4 lg:px-6">
-                      <ChartAreaInteractive />
+                    <div className="px-4 lg:px-6 text-foreground">
+                      {children}
                     </div>
-                    <DataTable data={data} /> */}
-                    {children}
                   </div>
                 </div>
               </div>
